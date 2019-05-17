368e2cdfcd12b7f54bf3fc732c23bc0c0dae5c6e
Wess58 <wesleymbate@gmail.com>
2019-05-17 13:51:23 +0300

    created get one maid by ID API

---
+++ get-maid.php
@@ -0,0 +1,38 @@
+<?php
+require_once "connect.php";
+
+$response = array();
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    $sql = "SELECT * FROM maids WHERE id = {$id}";
+
+    $conn = connect();
+
+    $result = $conn->query($sql);
+
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $response['id'] = $row['id'];
+        $response['name'] = $row['name'];
+        $response['msisdn'] = $row['msisdn'];
+        $response['email'] = $row['email'];
+        $response['description'] = $row['description'];
+        $response['address'] = $row['address'];
+
+        header('Content-Type: application/json');
+        http_response_code(200);
+        echo json_encode($response);
+    } else {
+        $response['status'] = 'failed';
+        $response['reason'] = 'No Record found';
+
+        header('Content-Type: application/json');
+        http_response_code(204);
+        echo json_encode($response);
+    }
+} else {
+    header('Content-Type: application/json');
+    http_response_code(400);
+}
